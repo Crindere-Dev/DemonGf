b420eac69097f73f5cb242ca80fab55dbdc15c48
Crindere-Dev <69606716+Crindere-Dev@users.noreply.github.com>
2021-11-17 16:17:37 +0000

    :)

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DemonGf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+        string[] first = new string[]{ "Lum", "Lex", "Lil","Za","Da", "Si","Lo", "Fa"};
+        string[] sec = new string[]{ "trix", "lix", "bex", "lysth", "osyth", "lith"};
+        Random gen = new Random();
+        int age = gen.Next(18, 999);
+        string[] gender = new string[]{"Male", "Female", "Non-Binary", "Trans Man", "Trans Woman", "Bigender","Agender", "Genderfluid"};
+        string[] Pronouns = new string[]{};
+
+        Console.WriteLine("Name:" + (first[rnd.Next(0, first.Length)])+(sec[rnd.Next(0, sec.Length)]));
+        Console.ReadLine();
+        System.Console.WriteLine("Age:" + age);
+        Console.ReadLine();
+        Console.WriteLine("Gender:"+ (gender[rnd.Next(0, gender.Length)]));
+        Console.ReadLine();
+ 
+        }
+    
+
+   private static void Love(string[] args)
+    {
+         
+      
+}
+    }}
